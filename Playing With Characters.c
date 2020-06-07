8d65c065f87aa403e36b4503ff854423d4ceb268
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-06-07 14:54:17 +0530

    Task is complete

---
+++ Playing With Characters.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char s[40],str[100],c;
+    
+    scanf("%c",&c);
+    scanf(" %s",s);
+    scanf(" %[^\n]%*c",str);
+    
+    printf("%c\n%s\n%s\n",c,s,str);
+    
+    return 0;
+}
