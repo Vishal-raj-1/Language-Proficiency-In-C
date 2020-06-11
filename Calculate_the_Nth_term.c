de158a27dd57d877c8c95fac6766be4ded8fab06
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-06-11 20:55:36 +0530

    Calculate_the_Nth_term.c added

--- Calculate_the_Nth_term.c
+++ Calculate_the_Nth_term.c
@@ -9,6 +9,7 @@ int find_nth_term(int n,int a,int b,int c)
     else if(n == 3)
         return c;
 
+    //Recursively call
     return find_nth_term(n-1,a,b,c) + find_nth_term(n-2,a,b,c) + find_nth_term(n-3,a,b,c);
 }
 
@@ -17,7 +18,6 @@ int main()
     int n,a,b,c;
 
     scanf("%d %d %d %d",&n,&a,&b,&c);
-
     int ans = find_nth_term(n,a,b,c);
 
     printf("%d\n",ans);
